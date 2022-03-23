f950df64a0ac74fc5df99df2de4bf952c453d1f2
rubio-e <49740649+rubio-e@users.noreply.github.com>
2022-03-22 22:40:19 -0600

    Update inf.R

--- inf.R
+++ inf.R
@@ -5,7 +5,7 @@ head(t, n = 5)
 
 mean(t$G1)
 length(t$G1)
-# Hipótesis de una cola
+# Hipótesis de una cola para comparación con media hipotética
 t.test(
   x = t$G1, # Variable peso
   mu = 23, # Media hipotética
@@ -18,12 +18,14 @@ boxplot(t$G1, t$G2, names = c("Grupo 1", "Grupo 2"))
 t.test(t$G1, t$G2, paired = F)
 t.test(extra ~ group, data = sleep)
 
+
+# Ejemplo anova
 Plantas <- read.csv("/home/rubio/MEGA/R_1/BDATOS/Plantas.csv", header = T) # Llamamos la base Plantas
 head(Plantas)
+Plantas$Grupo <- as.factor(Plantas$Grupo)
 plot(Peso ~ Grupo, data = Plantas) ## boxplot
 fligner.test(Peso ~ Grupo, data = Plantas)
-
-Plantas$Grupo <- as.factor(Plantas$Grupo)
+# ANOVA
 fit <- aov(Peso ~ (Grupo), data = Plantas) # Peso= Variable respuesta
 summary(fit)
 RES <- residuals(fit) # Calcula los residuales del modelo
@@ -31,7 +33,10 @@ plot(RES) # Muestra la distribución de los residuales en el gráfico
 qqnorm(RES)
 qqline(RES)
 shapiro.test(RES)
-
+# Post-Hoc
+TukeyHSD(fit, conf.level=0.95)
+plot(TukeyHSD(fit))
+# Usando agrocolae
 library(agricolae)
 out <- HSD.test(
   y = fit, trt = "Grupo",
